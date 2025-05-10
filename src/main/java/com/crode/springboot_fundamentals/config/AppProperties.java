436d1b51d8d09bdb1191c7dfda6b500d0ecbe43d
iuliana.dragoi <iulianadragoi95@gmail.com>
2025-05-10 12:57:16 +0300

    Implement Configuration properties class. Add it in application.properties. Configure in memory database (hibernate).

---
+++ src/main/java/com/crode/springboot_fundamentals/config/AppProperties.java
@@ -0,0 +1,11 @@
+package com.crode.springboot_fundamentals.config;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+@Component
+@ConfigurationProperties(prefix = "app")
+public class AppProperties {
+
+    private String version;
+}
