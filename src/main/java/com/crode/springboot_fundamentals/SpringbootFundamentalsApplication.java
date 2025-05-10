39cab3886e0a2ac2cc3125fae56d9d3a1846efad
iuliana.dragoi <iulianadragoi95@gmail.com>
2025-05-10 11:59:11 +0300

    Application Initialization

---
+++ src/main/java/com/crode/springboot_fundamentals/SpringbootFundamentalsApplication.java
@@ -0,0 +1,13 @@
+package com.crode.springboot_fundamentals;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootFundamentalsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootFundamentalsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/crode/springboot_fundamentals/SpringbootFundamentalsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.crode.springboot_fundamentals;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootFundamentalsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
