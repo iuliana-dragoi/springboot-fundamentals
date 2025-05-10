ff19d336ff598b09d7ae53cbf8bb22c26c20da1b
iuliana.dragoi <iulianadragoi95@gmail.com>
2025-05-10 12:39:24 +0300

    Add starters dependencies. Display all beans that are created. Add dev and prod application properties configurations.

--- src/main/java/com/crode/springboot_fundamentals/SpringbootFundamentalsApplication.java
+++ src/main/java/com/crode/springboot_fundamentals/SpringbootFundamentalsApplication.java
@@ -2,12 +2,23 @@ package com.crode.springboot_fundamentals;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+
+import java.util.Arrays;
 
 @SpringBootApplication
 public class SpringbootFundamentalsApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SpringbootFundamentalsApplication.class, args);
+		ConfigurableApplicationContext applicationContext = SpringApplication.run(SpringbootFundamentalsApplication.class, args);
+		displayBeans(applicationContext);
 	}
 
+	private static void displayBeans(ConfigurableApplicationContext applicationContext) {
+		String[] beans = applicationContext.getBeanDefinitionNames();
+		Arrays.sort(beans);
+		for(String bean : beans) {
+			System.out.println(" -> " + bean);
+		}
+	}
 }
