bd176daeffe2132fd1064f52505c9bdcdbb13ead
iuliana.dragoi <iulianadragoi95@gmail.com>
2025-05-10 20:57:56 +0300

    Implement Custom Query and Query Methods examples.

--- src/main/java/com/crode/springboot_fundamentals/repository/UserRepository.java
+++ src/main/java/com/crode/springboot_fundamentals/repository/UserRepository.java
@@ -3,9 +3,18 @@ package com.crode.springboot_fundamentals.repository;
 
 import com.crode.springboot_fundamentals.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+import java.util.List;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
 
+    @Query("Select u From User u JOIN u.roles r where r.name = :roleName")
+    List<User> findUsersByRoleName(@Param("roleName") String roleName);
+
+    List<User> findByAgeGreaterThan(int age);
+
+    List<User> findByAgeLessThan(int age);
 }
